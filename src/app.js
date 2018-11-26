3bf3e438535f223d43d4555fc3c61d6883eadbbb
Catriona Ford <catriona.b.ford@gmail.com>
2018-11-26 17:55:08 +0000

    app.js completed

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
+// require PrimeChecker model
+const PrimeChecker = require('./models/word_counter.js')
+// require views
+const FormView = require('./views/form_view.js')
+const ResultView = require('./views/result_view.js')
+
+// listen for:
+  // 1. events from the bind events function in the form view
+  // 2. events from the bind events function in the PrimeChecker model
+  // 3. events from the bind events function in the result view
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript Loaded');
+  const formView = new FormView();
+  formView.bindEvents();
+
+  const primeChecker = new PrimeChecker();
+  primeChecker.bindEvents();
+
+  const resultView = new ResultView();
+  resultView.bindEvents();
 });
